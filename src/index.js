bd5cf612cb1929dd4295354b1851ce6d57953061
WindWT <leibao0723@gmail.com>
2019-01-26 23:19:35 +0800

    bugfix

--- src/index.js
+++ src/index.js
@@ -5,21 +5,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './style.css'
 
-var timer;
-var initIframe = function () {
-    if ($('#news-container').is(':visible')) {
-        console.log('load news');
-        $('.news-iframe').each(function (i, iframe) {
-            var src = iframe.getAttribute('data-src');
-            iframe.src = src;
-        });
-    }
-};
+$(function () {
+    var timer;
+    var initIframe = function () {
+        if ($('#news-container').is(':visible')) {
+            console.log('load news');
+            $('.news-iframe').each(function (i, iframe) {
+                iframe.src = 'about:blank';
+                if ($(iframe).is(':visible')) {
+                    var src = iframe.getAttribute('data-src');
+                    iframe.src = src;
+                }
+            });
+        }
+    };
 
-window.onresize = function (event) {
-    clearTimeout(timer);
-    timer = setTimeout(initIframe, 500);
-};
-window.onload = function (event) {
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        $('.news-iframe').each(function (i, iframe) {
+            if ($(iframe).attr('src').indexOf('http') != 0 && $(iframe).is(':visible')) {
+                var src = iframe.getAttribute('data-src');
+                iframe.src = src;
+            }
+        });
+    })
+
+    window.onresize = function (event) {
+        clearTimeout(timer);
+        timer = setTimeout(initIframe, 500);
+    };
     initIframe();
-}
+})
